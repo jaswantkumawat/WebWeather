e53e360b41180374fcfb362d2848138e84ba0bce
jaswantkumawat <114277350+jaswantkumawat@users.noreply.github.com>
2023-04-19 13:14:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
+import React , {useState} from 'react';
 import './App.css';
+import axios from 'axios';
+import Webname from './components/webName/Webname';
 
 function App() {
+
+  const [data , setData] = useState({})
+  const [location , setLocation ] = useState('')
+
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=525a8d214702b9043b85fbd71afffe64`
+// https://api.openweathermap.org/data/2.5/weather?q={city name},{state code},{country code}&appid=525a8d214702b9043b85fbd71afffe64
+
+const searchLocation = (event) =>{
+  if(event.key === 'Enter'){
+    axios.get(url).then((response)=>{
+      setData(response.data)
+      console.log(response.data)
+  })
+  setLocation('')
+  }
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <>
+    <div className="app">
+    <Webname />
+      <div className="search">
+        <input
+          value={location}
+          onChange={event => setLocation(event.target.value)}
+          onKeyDown={searchLocation}
+          placeholder='Enter Location'
+          type="text" />
+      </div>
+      <div className="container">
+        <div className="top">
+          <div className="location">{data.name},{data.main ? <span>{data.sys.country}</span> : null} </div>
+          <div className="temp">
+            {data.main ? <h1>{data.main.temp}°C</h1> : null}
+          </div>
+          <div className="discription">
+            {data.weather ? <p>{data.weather[0].main}</p> : null}
+          </div>
+        </div>
+
+        {data.name !== undefined &&
+          <div className="bottom">
+            <div className="feels">
+              {data.main ? <p className='bold'>{data.main.feels_like}°C</p> : null}
+              <p>Feels Like</p>
+            </div>
+            <div className="humidity">
+              {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
+              <p>Humidty</p>
+            </div>
+            <div className="wind">
+              {data.main ? <p className='bold'>{Math.round(data.wind.speed * 1.609)} KM/H</p> : null}
+              <p>Wind Speed</p>
+            </div>
+          </div>}
+
+      </div>
     </div>
+    // </>
   );
 }
 
---
+++ src/components/webName/Webname.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import '../webName/Webname.css'
+import logo from '../../img/logo2.png'
+
+
+const Webname = () => {
+  return (
+    <span className="appName">
+       <img src={logo} alt="" />
+    </span>
+  )
+}
+
+export default Webname
+
+
